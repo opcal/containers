e39ee15765805be2a7d2d6323918ba772e723418
gissily <gissily@outlook.com>
2023-05-08 19:26:18 +0800

    buid image with buildx

--- tools/openssh-server/focal.sh
+++ tools/openssh-server/focal.sh
@@ -9,14 +9,13 @@ echo 'build openssh-server-focal start'
 IMAGE=ghcr.io/opcal/ubuntu:focal
 
 # openssh-server-focal
-docker build \
+docker buildx build \
+    --platform linux/amd64,linux/arm64/v8,linux/arm/v7,linux/ppc64le,linux/s390x \
     --build-arg BASE_IMAGE=${IMAGE} \
-    -t openssh-server:${TAG_VERSION} \
+    --push \
+    -t ${CI_REGISTRY}/opcal/openssh-server:focal-${LATEST_VERSION} \
+    -t ${CI_REGISTRY}/opcal/openssh-server:focal \
     -f ${PROJECT_DIR}/tools/openssh-server/base/Dockerfile . --no-cache
-docker image tag openssh-server:${TAG_VERSION} ${CI_REGISTRY}/opcal/openssh-server:focal-${LATEST_VERSION}
-docker image tag openssh-server:${TAG_VERSION} ${CI_REGISTRY}/opcal/openssh-server:focal
-docker push ${CI_REGISTRY}/opcal/openssh-server:focal-${LATEST_VERSION}
-docker push ${CI_REGISTRY}/opcal/openssh-server:focal
 
 echo 'build openssh-server-focal finished'
 echo " "
--- tools/openssh-server/jammy.sh
+++ tools/openssh-server/jammy.sh
@@ -9,14 +9,13 @@ echo 'build openssh-server-jammy start'
 IMAGE=ghcr.io/opcal/ubuntu:jammy
 
 # openssh-server-jammy
-docker build \
+docker buildx build \
+    --platform linux/amd64,linux/arm64/v8,linux/arm/v7,linux/ppc64le,linux/s390x \
     --build-arg BASE_IMAGE=${IMAGE} \
-    -t openssh-server:${TAG_VERSION} \
+    --push \
+    -t ${CI_REGISTRY}/opcal/openssh-server:jammy-${LATEST_VERSION} \
+    -t ${CI_REGISTRY}/opcal/openssh-server:jammy \
     -f ${PROJECT_DIR}/tools/openssh-server/base/Dockerfile . --no-cache
-docker image tag openssh-server:${TAG_VERSION} ${CI_REGISTRY}/opcal/openssh-server:jammy-${LATEST_VERSION}
-docker image tag openssh-server:${TAG_VERSION} ${CI_REGISTRY}/opcal/openssh-server:jammy
-docker push ${CI_REGISTRY}/opcal/openssh-server:jammy-${LATEST_VERSION}
-docker push ${CI_REGISTRY}/opcal/openssh-server:jammy
 
 echo 'build openssh-server-jammy finished'
 echo " "
