4170ed54f7e1294763c4b27322e0129dd567d527
gissily <gissily@outlook.com>
2023-05-08 23:26:20 +0800

    fix platform

--- openssh-server/jammy.sh
+++ openssh-server/jammy.sh
@@ -10,7 +10,7 @@ IMAGE=ghcr.io/opcal/ubuntu:jammy
 
 # openssh-server-jammy
 docker buildx build \
-    --platform linux/amd64,linux/arm64/v8,linux/arm/v7,linux/ppc64le,linux/s390x \
+    --platform linux/amd64,linux/arm64 \
     --build-arg BASE_IMAGE=${IMAGE} \
     --push \
     -t ${CI_REGISTRY}/opcal/openssh-server:jammy-${LATEST_VERSION} \
