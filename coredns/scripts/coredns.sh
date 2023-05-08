b2579c6a0c79f3bd28f28e4d70a5319a772e2802
gissily <gissily@outlook.com>
2023-05-08 22:57:42 +0800

    split coredns

--- coredns/scripts/coredns.sh
+++ coredns/scripts/coredns.sh
@@ -4,7 +4,7 @@ set -e
 
 echo " "
 echo " "
-echo 'build opcal-coredns start'
+echo 'build coredns start'
 
 export COREDNS_TAG=$(curl https://api.github.com/repos/coredns/coredns/releases/latest | grep tag_name | cut -d '"' -f 4)
 
@@ -14,10 +14,10 @@ docker buildx build \
     --platform linux/amd64,linux/arm64,linux/arm/v7,linux/ppc64le,linux/s390x \
     --build-arg COREDNS_TAG  \
     --push \
-    -t ${CI_REGISTRY}/opcal/opcal-coredns:${COREDNS_TAG} \
-    -t ${CI_REGISTRY}/opcal/opcal-coredns:latest \
-    -f ${PROJECT_DIR}/tools/coredns/Dockerfile . --no-cache
+    -t ${CI_REGISTRY}/opcal/coredns:${COREDNS_TAG} \
+    -t ${CI_REGISTRY}/opcal/coredns:latest \
+    -f ${PROJECT_DIR}/coredns/Dockerfile . --no-cache
 
-echo 'build opcal-coredns finished'
+echo 'build coredns finished'
 echo " "
 echo " "
