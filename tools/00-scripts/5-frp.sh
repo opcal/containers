5bb007a69559caf218844f53a1797c7f789bcf4f
gissily <gissily@outlook.com>
2023-05-08 18:36:16 +0800

    build with buildx

--- tools/00-scripts/5-frp.sh
+++ tools/00-scripts/5-frp.sh
@@ -8,23 +8,21 @@ echo 'build frp start'
 
 FRP_VERSION=$(curl https://api.github.com/repos/fatedier/frp/releases/latest | grep tag_name | cut -d '"' -f 4)
 
-docker build \
+docker buildx build \
+    --platform linux/amd64,linux/arm64 \
     --build-arg FRP_VERSION=${FRP_VERSION} \
-    -t frps:${TAG_VERSION} \
+    --push \
+    -t ${CI_REGISTRY}/opcal/frps:${FRP_VERSION} \
+    -t ${CI_REGISTRY}/opcal/frps:latest \
     -f ${PROJECT_DIR}/tools/frp/Dockerfile-server . --no-cache
-docker image tag frps:${TAG_VERSION} ${CI_REGISTRY}/opcal/frps:${FRP_VERSION}
-docker image tag frps:${TAG_VERSION} ${CI_REGISTRY}/opcal/frps:latest
-docker push ${CI_REGISTRY}/opcal/frps:${FRP_VERSION}
-docker push ${CI_REGISTRY}/opcal/frps:latest
 
-docker build \
+docker buildx build \
+    --platform linux/amd64,linux/arm64 \
     --build-arg FRP_VERSION=${FRP_VERSION} \
-    -t frpc:${TAG_VERSION} \
+    --push \
+    -t ${CI_REGISTRY}/opcal/frpc:${FRP_VERSION} \
+    -t ${CI_REGISTRY}/opcal/frpc:latest \
     -f ${PROJECT_DIR}/tools/frp/Dockerfile-client . --no-cache
-docker image tag frpc:${TAG_VERSION} ${CI_REGISTRY}/opcal/frpc:${FRP_VERSION}
-docker image tag frpc:${TAG_VERSION} ${CI_REGISTRY}/opcal/frpc:latest
-docker push ${CI_REGISTRY}/opcal/frpc:${FRP_VERSION}
-docker push ${CI_REGISTRY}/opcal/frpc:latest
 
 echo 'build frp finished'
 echo " "
